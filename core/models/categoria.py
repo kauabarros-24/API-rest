753fb24384c0fbaebd1c76bcf614fa9377034cd8
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-14 19:15:22 -0300

    fix: criado o campo nome e idade

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,3 +2,12 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+    #Adiciconado por Kaua
+    nome = models.CharField(max_length=50)
+    idade = models.IntegerField(default=True)
+    def __str__(self):
+        return self.descricao, self.nome
+    
+
+
+
