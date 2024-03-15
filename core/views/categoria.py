b605eeae3c1355534adfdbcd2749c9267f72a6fd
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-15 10:49:24 -0300

    created: categorias para view

---
+++ core/views/categoria.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    #define o conjunto de objetos que será retornado pela view.
+    queryset = Categoria.objects.all()
+    #define o serializer que será utilizado para serializar os objetos
+    serializer_class = CategoriaSerializer
+    
