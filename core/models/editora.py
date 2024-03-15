062046f320aeb2713921649e9ef6a342b6e9b7c4
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-15 13:13:26 -0300

    add: editora.py

---
+++ core/models/editora.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(max_length=200, blank=True, null=True)
+    #Setando o nome
+    def __str__(self):
+        return self.nome
+        
