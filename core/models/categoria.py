4ad6efe64c4a2f1c83d84a8d46aa2a30a9bb8746
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-15 12:49:37 -0300

    fixed: api voltou a funcionar, o problema era a ausência de default

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -3,12 +3,14 @@ from django.db import models
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
     #Adiciconado por Kaua
-    nome = models.CharField(max_length=50)
-    idade = models.IntegerField(default=True)
+    nome = models.CharField(max_length=50, default="Sem nome")
+    idade = models.IntegerField(default=10)
+    autor = models.CharField(max_length=100)
     #Não usar return self.x, self.y
     def __str__(self):
         return self.descricao
         return self.nome
+        return self.autor
     
 
 
