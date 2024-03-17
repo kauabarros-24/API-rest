6a6650cd8a4e7a767754320b8b1794e07ff780db
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-17 17:13:45 -0300

    fixed: serializer de listagem e detalhes criados

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -5,4 +5,13 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
         depth = 1
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ["id", "titulo", "categoria"]
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,7 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroSerializer, LivroListSerializer, LivroDetailSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
